255afb477132784f3329ee4530cafeba146bc38b
lixandro96 <80864384+lixandro96@users.noreply.github.com>
2023-07-13 21:21:50 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const url = `../data.json`;
+const summaryContainer = document.querySelector('.summary_container')
+window.addEventListener('DOMContentLoaded',() => {
+  loadData()
+})
+
+async function  loadData(){
+
+  const response = await fetch(url)
+  const data = await response.json()
+  showData(data)
+}
+function showData(data){
+  const summary = document.createElement('DIV');
+  summary.classList.add('summary')
+  data.forEach(element => {
+    const {category,score,icon} = element
+    summary.innerHTML += `
+      <div class="summary_item">
+        <p><img src="${icon}" alt="${category}">${category}</p>
+        <div>
+          <p>${score}<span class="light"> / 100</span></p>
+        </div>
+      </div>
+  
+    `
+    
+  });
+  summaryContainer.appendChild(summary)
+  const btn = document.createElement('DIV');
+  btn.classList.add('btn');
+  btn.innerHTML = `
+    <input type="button" value="Continue">
+  `
+  summaryContainer.appendChild(btn)
+}
